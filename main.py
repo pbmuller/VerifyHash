52f8f55caae4932bcc9266673bb1bb7436b3265e
Mat Boyles <bumpkin1987@gmail.com>
2021-06-02 17:20:53 -0400

    yeah my first commit boiiiis

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def print_hi(name):
 if __name__ == '__main__':
     # It would be super cool if we had a hash function with a public key
     # that is able to be verified but not reversed
+    # yo betch you fuck
     print_hi('PyCharm')
 
 
