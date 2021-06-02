062f89d55440f57fba1ef1a89215c4688995ef8d
Patrick <muller.98@osu.edu>
2021-06-02 17:14:57 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    # It would be super cool if we had a hash function with a public key
+    # that is able to be verified but not reversed
+    print_hi('PyCharm')
+
+
